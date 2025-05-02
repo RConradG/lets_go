cee28e249e3d8607d146581fc5ba43a68c935e20
RConradGe <rcgadduang@gmail.com>
2025-05-02 08:29:42 -0700

    wip: changed debug settings

--- lets_go/settings.py
+++ lets_go/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
-load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == 'TRUE'
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
