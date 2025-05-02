e4b157c370fa32a7c64dede3af21eff4bf97d801
RConradGe <rcgadduang@gmail.com>
2025-05-02 11:14:16 -0700

    wip: registered models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Collection, Item, Category
+from .models import Vendor, Event, SavedEvents, Address, Posts, Follows
 
-admin.site.register(Collection)
-admin.site.register(Item)
-admin.site.register(Category)
+admin.site.register(Vendor)
+admin.site.register(Event)
+admin.site.register(SavedEvents)
+admin.site.register(Address)
+admin.site.register(Posts)
+admin.site.register(Follows)
