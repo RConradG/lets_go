1a50684e74766b5b8bafc1655ff9af7e3319be32
RConradGe <rcgadduang@gmail.com>
2025-05-06 11:05:27 -0700

    wip: testing image upload

--- main_app/models.py
+++ main_app/models.py
@@ -2,20 +2,16 @@ from django.db import models
 from django import forms
 from django.urls import reverse
 from django.contrib.auth.models import User
-
-from phonenumber_field.modelfields import PhoneNumberField
+from cloudinary.models import CloudinaryField
 
 VENDOR_TYPES = (
-        ('F', 'Food'),
-        ('D', 'Drink'),
-        ('C', 'Crafts'),
+    ("F", "Food"),
+    ("D", "Drink"),
+    ("C", "Crafts"),
 )
 
-ROLES = (
-    ('U', 'USER'),
-    ('V', 'VENDOR'),
-    ('P', 'PROMOTER')
-)
+ROLES = (("U", "USER"), ("V", "VENDOR"), ("P", "PROMOTER"))
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -23,13 +19,14 @@ class UserProfile(models.Model):
     role = models.CharField(
         max_length=1,
         choices=ROLES,
-        default =ROLES[0][0],
+        default=ROLES[0][0],
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
+
 class Vendor(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='vendor')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="vendor")
     vendorType = models.CharField(
         max_length=1,
         choices=VENDOR_TYPES,
@@ -40,11 +37,12 @@ class Vendor(models.Model):
     business_email = models.EmailField(unique=False)
     # businessAddress = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='business_address')
     # phone_number = PhoneNumberField(region="US")
-    
-    
+
+
 class Event(models.Model):
-    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='event')
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name="event")
     title = models.CharField(max_length=100)
+    image = CloudinaryField('image')
     description = models.TextField(blank=True)
     location_name = models.CharField(max_length=100)
     location_street_number = models.IntegerField()
@@ -56,24 +54,31 @@ class Event(models.Model):
     end_date = models.DateField(null=True, blank=True)
     start_time = models.TimeField()
     end_time = models.TimeField()
-    saved_by = models.ManyToManyField(User, related_name='saved_events')
+    saved_by = models.ManyToManyField(User, related_name="saved_events")
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
+
 class Follows(models.Model):
-    followingUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
-    followedUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+    followingUser = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="following"
+    )
+    followedUser = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="followers"
+    )
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
+
 class Posts(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='posts')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="posts")
     title = models.CharField(max_length=100)
     body = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    
-class Image(models.Model):
-    url = models.CharField(max_length=200)
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return f"Photo for event_id: {self.event_id} @{self.url}"
+
+
+class TestModel(models.Model):
+    name = models.CharField(max_length=100)
+    image = CloudinaryField('image')
--- main_app/views.py
+++ main_app/views.py
@@ -6,11 +6,9 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Vendor, Event, Image
+from .models import Vendor, Event
 from .forms import EventForm, CustomSignUpForm
-import uuid
-import boto3
-import os
+
 
 def about(request):
     return render(request, "about.html")
@@ -27,16 +25,16 @@ def user_events(request):
     # Check if the user is a vendor
     try:
         vendor = Vendor.objects.get(user=request.user)
-        # If the user is a vendor, redirect to the vendor's events page
-        return redirect("vendor_events")  # Assuming 'vendor_events' is the URL for vendor events view
+        return redirect("vendor_events")
     except Vendor.DoesNotExist:
-        # If the user is not a vendor, show events for non-vendors
-        events = Event.objects.filter(vendor=None)  # Filter events for users who are not vendors
-        saved_events = request.user.saved_events.all()  # Assuming 'saved_events' is a related field on User model
-        return render(request, "user-events.html", {
-            "events": events,
-            "saved_events": saved_events
-        })
+        events = Event.objects.filter(vendor=None)
+        saved_events = request.user.saved_events.all()
+        return render(
+            request,
+            "user-events.html",
+            {"events": events, "saved_events": saved_events},
+        )
+
 
 @login_required
 def save_event(request, event_id):
@@ -49,21 +47,18 @@ def save_event(request, event_id):
         # Save the event
         request.user.saved_events.add(event)
 
-    return redirect('user-events')  # Redirect to the user-events page
+    return redirect("user-events")
 
-# View for vendors
+
 @login_required
 def vendor_events(request):
     try:
-        # Get the vendor profile of the logged-in user
         vendor = Vendor.objects.get(user=request.user)
-        # Fetch events related to the vendor
         events = Event.objects.filter(vendor=vendor)
         return render(
             request, "vendor-events.html", {"events": events, "vendor": vendor}
         )
     except Vendor.DoesNotExist:
-        # In case the user is not a vendor, redirect to the general user events page or show an error
         return redirect("user_events")
 
 
@@ -82,17 +77,15 @@ def event_detail(request, event_id):
     return render(
         request,
         "events/detail.html",
-        {
-            "event": event,
-            "event_form": event_form,
-            "vendor": vendor
-        },
+        {"event": event, "event_form": event_form, "vendor": vendor},
     )
-    
+
+
 @login_required
 def add_event(request):
+    vendor = Vendor.objects.filter(user=request.user).first()
     if request.method == "POST":
-        form = EventForm(request.POST)
+        form = EventForm(request.POST, request.FILES)
         if form.is_valid():
             new_event = form.save(commit=False)
 
@@ -109,7 +102,6 @@ def add_event(request):
                 )
     else:
         form = EventForm()
-        vendor = Vendor.objects.filter(user=request.user).first()
 
     return render(request, "events/event_form.html", {"form": form, "vendor": vendor})
 
@@ -140,38 +132,20 @@ def post_login_redirect(request):
         return redirect("user-events")
 
 
-def add_image(request, event_id):
-    image_file = request.FILES.get('image-file', None)
-    if image_file:
-        s3 = boto3.client('s3')
-        key = uuid.uuid4().hex[:6] + image_file.name[image_file.name.rfind('.'):]
-        
-        try:
-            bucket = os.environ['S3_BUCKET']
-            s3.upload_fileobj(image_file, bucket, key)
-            
-            url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
-            
-            Image.objects.create(url=url, event_id=event_id)
-        except Exception as e:
-            print('An error occurred uploading file to S3')
-            print(e)
-    return redirect("event-detail", event_id=event_id)
-
 class Home(LoginView):
     template_name = "home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['events'] = Event.objects.all()
+        context["events"] = Event.objects.all()
 
         if self.request.user.is_authenticated:
             try:
-                context['vendor'] = Vendor.objects.get(user=self.request.user)
+                context["vendor"] = Vendor.objects.get(user=self.request.user)
             except Vendor.DoesNotExist:
-                context['vendor'] = None
+                context["vendor"] = None
         else:
-            context['vendor'] = None
+            context["vendor"] = None
 
         return context
 
@@ -179,9 +153,10 @@ class Home(LoginView):
 class Login(LoginView):
     template_name = "login.html"
 
+
 class EventUpdate(UpdateView):
     model = Event
-    fields = "__all__"
+    form_class = EventForm
 
     def get_success_url(self):
         return reverse("vendor-events")
